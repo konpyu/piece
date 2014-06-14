34e0246adeb61376fc2f97035899190519ef605d
kon.yuichi <konpyu@gmail.com>
2014-06-14 22:02:10 +0900

    gem added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  helper_method :login?, :current_user
+
+  def error404
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500
+    render 'error500', status: 500, formats: [:html]
+  end
+
+  private
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def login?
+    session[:user_id].present?
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: 'ログインしてください'
+  end
+
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ApplicationController
+  before_filter :required_login
+
+  def index
+  end
+end
