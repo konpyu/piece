c98a2f2e9d97538003a30b6ed725d74a7841837d
kon.yuichi <konpyu@gmail.com>
2014-06-30 01:03:10 +0900

    test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_piece_session' , domain: :all, expire_after: 6.month
+Rails.application.config.session_store :cookie_store, key: '_piece_session' , expire_after: 6.month
